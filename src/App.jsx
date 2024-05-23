6e2ceaadfa652f170ddf026bcc2fbf0326799701
Mathanagopal R <appmechs@gmail.com>
2024-05-23 11:21:06 +0530

    Using Map function show the details

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import Hello from "./components/Hello";
-//Compenent App
-const App = () => {
-  const a=20;
-  const b=10;
-  const now = new Date();
-  const name ='Peter';
-return (
-  <div>
-    <Hello 
-    name={name}
-    age={30}
-    />
-    <h3>Hello World</h3>
-    Date:{now.toString()}
-    <p>{a} plus {b} is {a+b}</p>
-  </div>
-);
 
+const App = () => {
+  const friends =[
+    {id:1, name:'John', age:20},
+    {id:2, name:'Peter', age:30},
+  ]
+  return (
+    <div>
+      <h1>Friends</h1>
+      <ul>
+        {
+          friends.map( (friend) =>
+          <li key={friend.id}>{friend.name} {friend.age}</li>
+        )
+        }
+      </ul>
+      <ul>
+        {
+          friends.map( (friend, index) =>
+          <li key={index}>{friend.name} {friend.age}</li>
+        )
+        }
+      </ul>
+      </div>
+  )
 }
 
-export default App;
+export default App
