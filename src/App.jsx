05dee23aa648c32b452d2f90391dce587ee3a7ed
Mathanagopal R <appmechs@gmail.com>
2024-05-27 10:59:41 +0530

    class method using lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,55 @@
-import { Component } from "react";
-import Hello from "./components/Hello";
+import React, { Component } from 'react'
 
+export class App extends Component {
 
+  constructor(props){
+    super(props); //Call the constructor of the parent
+    this.state ={
+      counter:10
+    }
+  }
 
-class App extends Component {
-  // override the render method
+  componentDidMount() {
+    console.log('Compoen did mount');
+  }
+
+  componentDidUpdate() {
+    console.log('Compoen did Update');
+  }
+
+  componentWillUnmount() {
+    console.log('Compoen will unmount');
+  }
+
+  handeIncrement = () => {
+    //update the state using setState method
+    if(this.state.counter<19)
+      {
+    this.setState({
+      counter:this.state.counter+1
+    })
+  }
+  }
+  
+  handeDecrement = () => {
+    //update the state using setState method
+    this.setState({
+      counter:this.state.counter-1
+    })
+  }
+
   render() {
+    let counter =100;
+    console.log(this.state);
+  
     return (
       <div>
-      <Hello
-        name="John"
-      />
+        <p>COUNT: {this.state.counter}</p>
+        <button onClick={this.handeIncrement}>INCREMENT</button>
+        <button onClick={this.handeDecrement}>DECEMENT</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,11 +1,7 @@
-import { Component } from "react";
-class Hello extends Component{
-    render() {
-      return (
-        <div>
-        <h1>Hello {this.props.name}</h1>
-        </div>
-      );
-    }
-  }
-  export default Hello;
+import React from 'react'
+
+export const Hello = () => {
+  return (
+    <div>Hello</div>
+  )
+}
