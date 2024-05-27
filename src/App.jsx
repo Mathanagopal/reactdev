ff8c8b342648fd2a3d729b694cd63ec065566b4c
Mathanagopal R <appmechs@gmail.com>
2024-05-27 12:46:41 +0530

    use state using component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import Counter from "./components/Counter";
 
 const App = () => {
   let [count,seCount] = useState(0);
-  let [increment,setIncrement] = useState(0);
-
-  //runs only once when the component is mounted.
-  useEffect(() =>{
-    //console.log('Run when the component is mounted and count is updated');
-    console.log('useEffect called');
-    console.log('Count:', count);
-    console.log('Increment', increment)
-
-  },[count]);
+  
 
 const handleIncrement =() =>{
   seCount(count + 1);
-  setIncrement(increment+1);
 }
-const handleDecrement =() =>{
-  seCount(count - 1);
-}
-const handleRest =() =>{
-  seCount(0);
-}
+
 
   return (
     <div>
-      <h1>Counter: {count}</h1>
+     <Counter 
+      count={count}
+      />
     <button onClick={handleIncrement}>Increment</button>
-    <button onClick={handleDecrement}>Decrement</button>
-    <button onClick={handleRest}>Reset</button>
     </div>
   )
 }
